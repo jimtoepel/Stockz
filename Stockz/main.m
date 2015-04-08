79f40c951e0f28973a7b02ffa5591d94f5345261
jimtoepel <jim.toepel@gmail.com>
2015-04-07 20:06:25 -0700

    First try

--- Stockz/main.m
+++ Stockz/main.m
@@ -10,8 +10,28 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        
+        NSMutableArray *stocks = [[NSMutableArray alloc] init];
+        
+        NSMutableDictionary *stock;
+        
+        stock = [NSMutableDictionary dictionary];
+        [stock setObject:@"AAPL"
+                  forKey:@"symbol"];
+        [stock setObject:[NSNumber numberWithInt:200]
+                  forKey:@"shares"];
+        [stocks addObject:stock];
+         
+         stock = [NSMutableDictionary dictionary];
+         [stock setObject:@"GOOG"
+                   forKey:@"symbol"];
+         [stock setObject:[NSNumber numberWithInt:160]
+                   forKey:@"shares"];
+         [stocks addObject:stock];
+         
+        [stocks writeToFile:@"/tmp/stocks.plist"
+                 atomically:YES];
+        
     }
     return 0;
 }
