2a63d57a30e8bd668666a48a37d5f17459ac49cf
jimtoepel <jim.toepel@gmail.com>
2015-04-07 19:59:44 -0700

    Initial commit

---
+++ Stockz/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Stockz
+//
+//  Created by Jim Toepel on 4/7/15.
+//  Copyright (c) 2015 FunderDevelopment. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
